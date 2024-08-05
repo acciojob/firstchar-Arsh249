4e8c2eae5228bcbd92ddef6148310a5f9d9c577e
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-05 16:43:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function firstChar(text) {
-  // your code here
+function firstChar(str) {
+    // Loop through each character in the string
+    for (let i = 0; i < str.length; i++) {
+        // Check if the current character is not a space
+        if (str[i] !== ' ') {
+            return str[i]; // Return the first non-space character
+        }
+    }
+    // If no non-space character is found, return an empty string
+    return '';
 }
 
 // Do not change the code below
